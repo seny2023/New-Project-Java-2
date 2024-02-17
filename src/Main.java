fa4580d38557a7c6b573c997fbcca9af1345665d
Test new <yaguar487@gmail.con>
2024-02-17 17:04:05 +0400

    Gitignore added

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+
+        int price = 13_676;
+
+        int miles = price / 20;
+        System.out.println("Бонус " + miles);
+    }
 }
